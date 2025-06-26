dc9c1ae4f543425e2d5fb0cea3753b8200f8ad5d
Gauransh <gouranshsattavan@gmail.com>
2025-06-26 14:40:28 +0530

    Add files via upload

---
+++ emailjs-config.js
@@ -0,0 +1,11 @@
+// EmailJS Configuration
+// Replace these with your actual EmailJS keys
+
+const EMAILJS_CONFIG = {
+    publicKey: 'YOUR_PUBLIC_KEY',     // From EmailJS Account -> API Keys
+    serviceId: 'YOUR_SERVICE_ID',     // From your Gmail service  
+    templateId: 'YOUR_TEMPLATE_ID'    // From your email template
+};
+
+// Don't change anything below this line
+window.EMAILJS_CONFIG = EMAILJS_CONFIG;
